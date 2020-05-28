7f7812c7dece28f1383c3d324802a0b31ea0357d
iarafat <miyasinarafat@gmail.com>
2020-05-28 19:33:39 +0600

    added add to cart option

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,16 +4,14 @@
     <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif">
     <ul v-else>
       <li v-for="product in products" :key="product.id">
-        {{ product.title }} - {{ product.price }}
+        {{ product.title }} - {{ product.price }} - {{ product.inventory }}
+        <button @click="addProductToCart(product)">Add to cart</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-  import store from "@/store/index";
-
-
     export default {
       data() {
         return {
@@ -21,14 +19,21 @@
         }
       },
 
-      computed: {
-        products() {
-          return store.getters.availableProducts;
+      methods: {
+        addProductToCart(product) {
+          this.$store.dispatch('addProductToCart', product);
         }
       },
+
+      computed: {
+        products() {
+          return this.$store.getters.availableProducts;
+        }
+      },
+
       created() {
         this.loading = true
-        store.dispatch('fetchProducts')
+        this.$store.dispatch('fetchProducts')
         .then(() => this.loading = false);
       }
     }
--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: { // = data
-    products: []
+    products: [],
+    // {id, quantity}
+    cart: [],
   },
 
   getters: { // = computed properties
@@ -26,12 +28,37 @@ export default new Vuex.Store({
         });
       });
     },
+
+    addProductToCart(context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id);
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id);
+        } else {
+          context.commit('incrementItemQuantity', cartItem);
+        }
+
+        context.commit('decrementProductInventory', product);
+      }
+    },
   },
 
   mutations: { // = update and set the instance
     setProducts(state, products) {
       // update products
       state.products = products
+    },
+    pushProductToCart(state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++
+    },
+    decrementProductInventory(state, product) {
+      product.inventory--
     }
   }
 
