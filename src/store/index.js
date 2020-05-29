fff06dec00d984461af13a7da00adf4a41cffe50
iarafat <miyasinarafat@gmail.com>
2020-05-29 16:39:45 +0600

    split actions form vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
-import shop from "@/api/shop";
+import actions from "./actions";
 
 Vue.use(Vuex);
 
@@ -39,43 +39,7 @@ export default new Vuex.Store({
     }
   },
 
-  actions: { // = methods
-    fetchProducts(context) {
-      // make the api call
-      // call setProducts mutation
-      return new Promise((resolve, reject) => {
-        shop.getProducts(products => {
-          context.commit('setProducts', products);
-          resolve();
-        });
-      });
-    },
-
-    addProductToCart({state, commit, getters}, product) {
-      if (getters.productInStock(product)) {
-        const cartItem = state.cart.find(item => item.id === product.id);
-        if (!cartItem) {
-          commit('pushProductToCart', product.id);
-        } else {
-          commit('incrementItemQuantity', cartItem);
-        }
-        commit('decrementProductInventory', product);
-      }
-    },
-
-    checkout({state, commit}) {
-      shop.buyProducts(
-        state.cart,
-        () => {
-          commit('emptyCart')
-          commit('setCheckoutStatus', 'success')
-        },
-        () => {
-          commit('setCheckoutStatus', 'fail')
-        }
-        )
-    },
-  },
+  actions: actions,
 
   mutations: { // = update and set the instance
     setProducts(state, products) {
